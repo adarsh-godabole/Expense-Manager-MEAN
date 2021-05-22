f5fdd681cb803d773ecc62731f4aa1a690202fe8
adarsh-godabole <41135550+adarsh-godabole@users.noreply.github.com>
2021-05-22 16:29:38 +0530

    Add Expense UI created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'exp-manager';
+  title='Expense Manager'
+
 }
---
+++ src/app/expense-list.ts
@@ -0,0 +1,6 @@
+import { Expense } from './expense';
+
+export const EXPENSES: Expense[] = [
+  { name : 'Movie', amount : 300 },
+  {name : 'Groceries', amount : 100 },
+];
