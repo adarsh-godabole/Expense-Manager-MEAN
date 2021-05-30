41ac97795108221a1e3bb508706d6e55ce85cb4e
adarsh-godabole <41135550+adarsh-godabole@users.noreply.github.com>
2021-05-30 22:55:10 +0530

    Chart Added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -13,11 +13,18 @@ import { Chart } from 'chart.js';
 })
 export class DashboardComponent implements OnInit {
 
+  FuelAmount: number = 0
+  EntertainmentAmount: number = 0
+  GrocerieslAmount: number = 0
+  MedicineAmount: number = 0
+  HouseholdeAmount: number = 0
+  a = 10
+
   canvas: any;
   ctx: any;
   @ViewChild('mychart') mychart: any;
 
-  ngAfterViewInit() {
+  Draw() {
     this.canvas = this.mychart.nativeElement;
     this.ctx = this.canvas.getContext('2d');
 
@@ -25,17 +32,21 @@ export class DashboardComponent implements OnInit {
       type: 'pie',
       data: {
         labels: [
-          'Red',
-          'Blue',
-          'Yellow'
+          'Fuel',
+          'Entertainment',
+          'Groceries',
+          'Medicine',
+          'Household'
         ],
         datasets: [{
           label: 'My First Dataset',
-          data: [300, 50, 100],
+          data: [this.FuelAmount, this.EntertainmentAmount, this.GrocerieslAmount, this.MedicineAmount, this.HouseholdeAmount],
           backgroundColor: [
             'rgb(255, 99, 132)',
             'rgb(54, 162, 235)',
-            'rgb(255, 205, 86)'
+            'rgb(255, 205, 86)',
+            'rgb(25, 205, 86)',
+            'rgb(255, 25, 86)'
           ],
 
           fill: true,
@@ -61,7 +72,36 @@ export class DashboardComponent implements OnInit {
         this.recentExpenses = Array.from(this.expenses);
         this.expenses.sort((a, b) => b.amount - a.amount);
         this.recentExpenses.reverse();
+        this.caluclateAmountpercategory();
+
       })
+  }
+
+  caluclateAmountpercategory() {
+    // this.expenses.forEach(exp => console.log(exp))
+    this.expenses.forEach(
+      (exp) => {
+        if (exp.category === "Groceries") {
+          this.GrocerieslAmount += exp.amount;
+        }
+        if (exp.category === "Household") {
+          this.HouseholdeAmount += exp.amount;
+        }
+        if (exp.category === "Fuel") {
+          this.FuelAmount += exp.amount;
+        }
+        if (exp.category === "Entertainment") {
+          this.EntertainmentAmount += exp.amount;
+        }
+        if (exp.category === "Medicine") {
+          this.MedicineAmount += exp.amount;
+        }
+
+      }
+    )
+    console.log(this.EntertainmentAmount);
+
+    this.Draw();
   }
 
 }
